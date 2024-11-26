3cbb053d725acec358f17e37929172242c7247aa
SujithR03 <ds245229147@bhc.edu.in>
2024-11-26 18:24:19 +0530

    Add files via upload

---
+++ week_1.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # program_1 - arithmetic operations
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+arr=np.array([1,2,3,4,5])
+
+
+# In[3]:
+
+
+print("array:",arr)
+
+
+# In[4]:
+
+
+print("add 5:",arr+5)
+
+
+# In[5]:
+
+
+print("multiply by 2:",arr*2)
+
+
+# # program_2 - statistical operations
+
+# In[6]:
+
+
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8,9,10])
+print("mean:",np.mean(arr))
+print("median:",np.median(arr))
+print("standard deviation:",np.std(arr))
+
+
+# # program_3 - reshaping arrays
+
+# In[7]:
+
+
+import numpy as np
+arr=np.arange(1,13)
+reshape_arr=arr.reshape(3,4)
+print("reshaped array:",reshape_arr)
+
+
+# # program_4 - array indexing and slicing
+
+# In[8]:
+
+
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("first element:",arr[0])
+print("last element:",arr[-1])
+print("slice(1 to 3):",arr[1:4])
+
+
+# # program_5 - array concatenation
+
+# In[9]:
+
+
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+con=np.concatenate((arr1,arr2))
+print("concatenated array:",con)
+
+
+# # program_6 - boolean indexing
+
+# In[10]:
+
+
+import numpy as np
+arr=np.array([1,2,3,4,5])
+filt_arr=arr[arr>2]
+print("filtered array:",filt_arr)
+
+
+# # program_7 - dot product
+
+# In[11]:
+
+
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+dot_product=np.dot(arr1,arr2)
+print("dot product",dot_product)
+
+
+# # program_8 - linear algebra
+
+# In[14]:
+
+
+import numpy as np
+matrix=np.array([[1,2],[3,4]])
+determinant=np.linalg.det(matrix)
+inverse=np.linalg.inv(matrix)
+print("determinant",determinant)
+print("inverse",inverse)
+
+
+# In[ ]:
+
+
+
+
